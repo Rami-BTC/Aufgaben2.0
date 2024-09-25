909f19d45e40dbf91be2d347ab95149d2ba4c902
Abdullatif <rami.abdullatif@btc-ag.com>
2024-09-25 16:17:47 +0200

    'Sortieren' Code hinzugefügt

--- src/main/java/com/btcag/bootcamp/Sortieren.java
+++ src/main/java/com/btcag/bootcamp/Sortieren.java
@@ -1,4 +1,24 @@
 package com.btcag.bootcamp;
 
 public class Sortieren {
+    public static void main(String[] args) {
+        int[] list = {5,13,6,8,11};
+
+        for (int i = 0; i < list.length - 1; i++) {
+            if (list[i+1] < list[i]){
+                int tmp = list[i+1];
+                list[i+1] = list[i];
+                list[i] = tmp;
+            }
+        }
+
+        System.out.print("[");
+        for (int i = 0; i < list.length; i++) {
+            System.out.print(list[i]);
+            if (i < list.length - 1){
+                System.out.print(",");
+            }
+        }
+        System.out.print("]");
+    }
 }
