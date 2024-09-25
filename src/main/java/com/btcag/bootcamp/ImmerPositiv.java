dffd480a976ca12da02ca135b15b6a08b15a6df4
Abdullatif <rami.abdullatif@btc-ag.com>
2024-09-25 12:18:30 +0200

    ImmerPositiv-Aufgabencode Hinzugefügt

--- src/main/java/com/btcag/bootcamp/ImmerPositiv.java
+++ src/main/java/com/btcag/bootcamp/ImmerPositiv.java
@@ -1,7 +1,18 @@
 package com.btcag.bootcamp;
 
+import java.util.Scanner;
+
 public class ImmerPositiv {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Gebe eine ganze Zahl ein: ");
+        int num = sc.nextInt();
 
+        System.out.print("Die positive Zahl ist: " + convertToPos(num));
+    }
+
+    public static int convertToPos(int num){
+        if (num < 0){ return num * (-1);
+        } else { return num;}
     }
 }
