857c61a8dbbfb10e3cb054ce9c7b731d4190315b
Abdullatif <rami.abdullatif@btc-ag.com>
2024-09-30 09:23:13 +0200

    Methoden Hinzugefügt

--- src/main/java/com/btcag/bootcamp/Sortieren.java
+++ src/main/java/com/btcag/bootcamp/Sortieren.java
@@ -4,6 +4,11 @@ public class Sortieren {
     public static void main(String[] args) {
         int[] list = {5,13,6,8,11};
 
+        sortArray(list);
+        printArray(list);
+    }
+
+    private static void sortArray(int[] list) {
         for (int i = 0; i < list.length - 1; i++) {
             if (list[i+1] < list[i]){
                 int tmp = list[i+1];
@@ -11,7 +16,9 @@ public class Sortieren {
                 list[i] = tmp;
             }
         }
+    }
 
+    private static void printArray(int[] list) {
         System.out.print("[");
         for (int i = 0; i < list.length; i++) {
             System.out.print(list[i]);
